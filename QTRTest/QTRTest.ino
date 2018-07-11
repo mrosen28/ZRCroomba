bafbc105fe2a2f6fe1ca9080c82cbe50e1635629
System Administrator <root@Michaels-MacBook-Pro.local>
2018-07-11 17:01:32 -0400

    Update Arduino Code... again.

--- QTRTest/QTRTest.ino
+++ QTRTest/QTRTest.ino
@@ -1,31 +1,39 @@
 #include <QTRSensors.h>
+#define NUMBEROFSENSORS 8
 #define EMITTER_PIN 11
 #define TIMEOUT 2500
-//#define DEBUG
-unsigned int sensorReading[8];/*number of sensors*/
-QTRSensorsRC qtr((unsigned char[]){3, 4, 5, 6, 7, 8, 9, 10}, 8/*number of sensors*/, TIMEOUT, EMITTER_PIN);
+#define BAUDRATE 115200
+#define DEBUG //DEBUG: No Calibration, Autosend Reading on One Line
+              //Non-DEBUG: Calibrate, Wait for Connection Byte in Setup, Send Data upon Request
+unsigned int sensorReading[NUMBEROFSENSORS];
+QTRSensorsRC qtr((unsigned char[]){3, 4, 5, 6, 7, 8, 9, 10}, NUMBEROFSENSORS, TIMEOUT, EMITTER_PIN);
 void setup()
 {
   pinMode(13, OUTPUT);
   digitalWrite(13, HIGH);
-  Serial.begin(9600);
+  Serial.begin(BAUDRATE);
 
-  #ifdef DEBUG
+  #ifndef DEBUG
   for (int i = 0; i < 400; i++)
     qtr.calibrate();//reads all sensors 10 times at 2500 us per read (i.e. ~25 ms per call)
+
+   while (!Serial.available()){
+    Serial.write(0);
+   }
+  #else
+    Serial.print("DEBUG Mode: Skipping QTR Calibration.");
   #endif
 
-  while (!Serial.available()){
-    Serial.write(0);
-  }
+
   digitalWrite(13,LOW);
 }
 void loop()
 {
   #ifndef DEBUG
   while(!Serial.available());
-  while(Serial.available())
+  while(Serial.available()){
     Serial.read();
+  }
   #endif
   unsigned char out = 0;
   qtr.read(sensorReading);
@@ -34,7 +42,7 @@ void loop()
     out *= 2;
     out += r > 1000;
   }
-  #ifdef DEBUG
+  #ifdef DEBUG //Print Out Entire Sensor Reading on New Line
   for(int i=0;i<8;i++)
     Serial.print(bitRead(out,7-i));
   Serial.println();
--- main.py
+++ main.py
@@ -18,35 +18,6 @@ def get_serial_connection(baudrate):
             print("No Ports Found! Please try again...")
             continue
 
-def line_reader():
-    s=get_serial_connection()
-    def get():
-        s.write(b' ')
-        out=s.read()
-        s.read_all()
-        out=bin(ord(out))[2:]
-        return list(map(bool,map(int,'0'*(8-len(out))+out)))
-    def getTilt():
-        #print(' '.join(['X' if x else '-' for x in get()]))
-        def on_the_line(l):
-            l=''.join(map(str,map(int,l)))
-            if not '1' in l:return False
-          #  print(l)
-            for sub in '111 11 1'.split(' '):
-                if sub in l:
-                    l=l.replace(sub,'',1)
-                    break
-            return '1' not in l
-        L=get()
-        print (' '.join(['X' if x else '-' for x in L])), 'ON THE LINE' if on_the_line(L) else 'offline'
-        if on_the_line(L):
-            w=[]
-            for i in range(8):
-                if L[i]:w.append(i)
-            w=sum(w)/float(len(w))
-            return (-1+2*float(w)/7)
-    return getTilt
-front_tilt=line_reader(9600)
 roomba=get_serial_connection(115200)
 
 def to_bytes(n, length, endianess='big'):
@@ -63,20 +34,17 @@ def twos_complement(val, nbits):
             # compute negative value.
             val = val - (1 << nbits)
     return val
+
 def to_signed_short(n,number_of_bytes=2):
     assert -32768<=n<=32767,'to_signed_short: error: '+str(n)+' is not a valid signed short'
     return to_bytes(twos_complement(n,8*number_of_bytes),number_of_bytes)
+
 def set_motors(left,right):
-
     roomba.write(bytes(chr(146))+to_signed_short(left)+to_signed_short(right))
-
-def
-bytes(''.join(chr(int(x))for x in raw_input("Enter Command: ").split(' ')))
 
 while True:
     try:
-        b=bytes(''.join(chr(int(x))for x in raw_input("Enter Command: ").split(' ')))
-        print("Sent: "+repr(b))
+        exec(raw_input(">>> "))
         connection.write(b)
     except Exception:
         print("Bad Input! Error: "+str(Exception))
